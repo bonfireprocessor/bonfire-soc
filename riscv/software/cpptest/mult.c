d369c5eb343a3b8a2b10e8ebac6c8b2cb816cf4e
Thomas Hornschuh <thomas@hornschuh-family.de>
2016-09-27 18:28:18 +0200

    Added riscv C test programs (only for testing compiler yet...)

---
+++ riscv/software/cpptest/mult.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+volatile uint8_t *gpioadr=(uint8_t *)0x10000000;
+
+int main(int argc,char ** argv) {
+
+volatile int counter =0;
+
+    while(1) {
+        *gpioadr= ((5*counter++) >> 20) & 0x0f;
+   }
+
+};
