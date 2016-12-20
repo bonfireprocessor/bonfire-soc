fbca6df487a78a8d44bc235f9374d32a4c102e4d
Thomas Hornschuh <thomas@hornschuh-family.de>
2016-12-20 23:55:51 +0100

    Update memory test programs

--- software/wildfire/test/memtest.c
+++ software/wildfire/test/memtest.c
@@ -4,13 +4,13 @@
 
 #include "mempattern.h"
 
-//char *pRAM = (char*)0x30000000; // Memory address
+
 
 extern uint32_t _rombase;
 
-#define MEMSIZE 4096*4; // Mem Size in Bytes
+//#define MEMSIZE 4096*4; // Mem Size in Bytes
 
-char *Welcome = "Hello World \r\n";
+#define MEMSIZE 8192*1024 / 4 // 8 Megabytes 
 
 void strcpy(char *t,char *s)
 {
@@ -35,37 +35,53 @@ int i;
     }
 }
 
+
+void doTest()
+{
+int errors;
+char buff[32];
+   
+   itoa(MEMSIZE,buff,10);
+   writestr(buff);
+   writestr("\r\nWriting test pattern...\r\n"); 
+   writepattern(DRAM_BASE,MEMSIZE);
+   writestr("Verifying test pattern...\r\n");
+   errors=verifypattern(DRAM_BASE,MEMSIZE);
+   itoa(errors,buff,10);
+   writestr("Number of errors: ");writestr(buff); writestr("\r\n");
+    
+}
+
 int main()
 {
 uint32_t *memptr=  DRAM_BASE;
 char c;
 const int blocksz = 64;
-int errors;
+//int errors;
 char buff[32];
 
    //setDivisor(16);
    setBaudRate(115200);
-   writestr("Memory test program 2.0\r\nProcessor ID: ");
+   writestr("Memory test program 3.0\r\nProcessor ID: ");
    writeHex(get_impid());
    writestr("\r\nUART Divisor: ");
    itoa(getDivisor(),buff,10);
    writestr(buff);
    writestr("\r\n");
    
+   doTest();
    
    while(1) {
        
-     writepattern(memptr,blocksz);
-     errors=verifypattern(memptr,blocksz);
-     itoa(errors,buff,10);
-     writestr("Number of errors: ");writestr(buff); writestr("\r\n");
-      
-     HexDump(memptr,64);
-     writestr("\r\npress r to restart with address 0, any other key to continue\r\n");
+        
+           
+     HexDump(memptr,blocksz);
+     writestr("\r\npress r to restart test, any other key to continue dump\r\n");
      c=readchar();
-     if ( (c=='r') || (c=='R'))
+     if ( (c=='r') || (c=='R')) {
+       doTest();  
        memptr= DRAM_BASE;
-     else
-       memptr+=64;
+     } else
+       memptr+=blocksz;
    }
 }
